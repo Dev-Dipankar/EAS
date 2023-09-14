2e21aae351145ea73334c2c0c0cc3de66a5710d9
Dev-Dipankar <dipankarjoshi.rl@gmail.com>
2023-09-14 10:33:27 +0545

    Correction on register.php

--- admin/register.php
+++ admin/register.php
@@ -1,6 +1,6 @@
 <?php
     include "../database/dbconnect.php";
-    include "../secure/register_be_validate.php";
+    include "../database/register_be.php";
 ?>
 
 <!DOCTYPE html>
--- eas_db.sql
+++ eas_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2023 at 05:06 AM
+-- Generation Time: Sep 14, 2023 at 06:43 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -78,9 +78,23 @@ CREATE TABLE `emp_info` (
 --
 
 INSERT INTO `emp_info` (`emp_id`, `dept_id`, `emp_name`, `emp_address`, `emp_gender`, `emp_phone`, `emp_email`, `department`, `emp_photo`) VALUES
-(2, 1, 'dipankar joshi', 'satungal', '', '985674', 'dipankar@gmail.com', 1, ''),
-(3, 2, 'aagya shrestha', 'kritipur', '', '9860458124', 'aagya@gmail.com', 1, ''),
-(9, 3, 'Riya Shrestha', 'Chapagau', '', '9856422317', 'riya@gmail.com', 2, '');
+(1, 1, 'Dipankar Joshi', 'Satungal', '', '9860254623', 'dipankar@gmail.com', 1, ''),
+(2, 1, 'Aagya Shrestha ', 'Lubhu', '', '9860125475', 'aayga@gmail.com', 1, ''),
+(3, 3, 'Alan Khanal', 'jhapa', '', '9825461254', 'alan@gmail.com', 3, ''),
+(4, 2, 'Riya Shrestha', 'Naikap', '', '9865412574', 'riya@gmail.com', 2, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scan_records`
+--
+
+CREATE TABLE `scan_records` (
+  `id` int(11) NOT NULL,
+  `qr_code_data` varchar(255) NOT NULL,
+  `time_in` datetime DEFAULT NULL,
+  `time_out` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -91,9 +105,20 @@ INSERT INTO `emp_info` (`emp_id`, `dept_id`, `emp_name`, `emp_address`, `emp_gen
 CREATE TABLE `super_admin` (
   `admin_id` int(15) NOT NULL,
   `admin_name` varchar(60) NOT NULL,
-  `email_id` varchar(60) NOT NULL,
-  `passwd` varchar(100) NOT NULL
+  `admin_address` varchar(30) NOT NULL,
+  `admin_email` varchar(60) NOT NULL,
+  `admin_phone` bigint(10) NOT NULL,
+  `admin_passwd` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `super_admin`
+--
+
+INSERT INTO `super_admin` (`admin_id`, `admin_name`, `admin_address`, `admin_email`, `admin_phone`, `admin_passwd`) VALUES
+(1, 'Dipankar Joshi', 'Satungal', 'dipankar@gmail.com', 9860325648, 'root@123'),
+(2, 'Aagya Shrestha', 'Lubhu', 'aagya@gmail.com', 9856125412, 'root123'),
+(3, 'riya', 'naikap', 'riya@gmail.com', 9856125412, 'riya123');
 
 --
 -- Indexes for dumped tables
@@ -118,10 +143,17 @@ ALTER TABLE `emp_info`
   ADD PRIMARY KEY (`emp_id`);
 
 --
+-- Indexes for table `scan_records`
+--
+ALTER TABLE `scan_records`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `super_admin`
 --
 ALTER TABLE `super_admin`
-  ADD PRIMARY KEY (`admin_id`);
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `admin_email` (`admin_email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -137,7 +169,19 @@ ALTER TABLE `department`
 -- AUTO_INCREMENT for table `emp_info`
 --
 ALTER TABLE `emp_info`
-  MODIFY `emp_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `emp_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `scan_records`
+--
+ALTER TABLE `scan_records`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `super_admin`
+--
+ALTER TABLE `super_admin`
+  MODIFY `admin_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
